136e65ceeb6480b965eb33a36dd406ed5056a922
Neyzer <neyzertoledo@gmail.com>
2026-07-21 01:38:01 -0700

    refactor: extend refresh token lifetime

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -150,7 +150,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
