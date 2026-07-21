3f5b137b003c9b8483c61f7169ee4c5eedf84aae
Neyzer <neyzertoledo@gmail.com>
2026-07-20 23:36:40 -0700

    feat: django admin use User model

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('user', 'name', 'lastname', 'active', 'is_staff', 'created_at')
+    search_fields = ('user', 'name', 'lastname')
+    ordering = ('-created_at',)
+    list_filter = ('active', 'is_staff', 'is_superuser')
+    filter_horizontal = ()
+
+    fieldsets = (
+        (None, {'fields': ('user', 'password')}),
+        ('Personal info', {'fields': ('name', 'lastname', 'school_id')}),
+        ('Permissions', {'fields': ('active', 'is_staff', 'is_superuser')}),
+        ('Important dates', {'fields': ('created_at', 'updated_at')}),
+    )
+    readonly_fields = ('created_at', 'updated_at')
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('user', 'name', 'lastname', 'password1', 'password2'),
+        }),
+    )
