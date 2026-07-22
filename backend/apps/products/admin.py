b7a0758128e19599793c6205f61eabd04606c60d
Neyzer <neyzertoledo@gmail.com>
2026-07-21 18:19:01 -0700

    feat: admin view and migration

--- backend/apps/products/admin.py
+++ backend/apps/products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'image', 'active', 'created_at', 'updated_at')
+    list_filter = ('active',)
+    search_fields = ('name', 'description')
+    readonly_fields = ('created_at', 'updated_at')
