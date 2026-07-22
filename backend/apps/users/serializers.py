8f98349194d2a8b62d93d145f08000cb7cbb87a8
GeneBrit <0323105958@ut-tijuana.edu.mx>
2026-07-21 22:19:00 -0700

    feat: create-clients endpoint done

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import User
 import re
 
-
 class LoginSerializer(serializers.Serializer):
     user = serializers.CharField(max_length=254)
     password = serializers.CharField(max_length=255, write_only=True)
@@ -37,7 +36,7 @@ class CreateClientSerializer(serializers.Serializer):
     def validate(self, data):
         user = data.get('user')
 
-        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.edu\.mx$"
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.edu\.mx$" ## para validar que sea un correo institucional válido
         if not re.match(pattern, user):
             raise serializers.ValidationError("Debe utilizar un correo institucional.")
 
