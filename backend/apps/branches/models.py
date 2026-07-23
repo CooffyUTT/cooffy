8594f493fb477a4418917066405a885bc3ac7bfa
Jabesll21 <0323105909@ut-tijuana.edu.mx>
2026-07-22 21:17:55 -0700

    agrege modelo Branch y su migración

--- backend/apps/branches/models.py
+++ backend/apps/branches/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Branch(models.Model):
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=255, blank=True, null=True)
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    school_id = models.BigIntegerField()
+    active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'branches'
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.name
