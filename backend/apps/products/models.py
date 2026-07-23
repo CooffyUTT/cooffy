8387722934d38f2878b1bd1295a6a734919f06b3
Neyzer <neyzertoledo@gmail.com>
2026-07-22 23:31:45 -0700

    feat(images): model and url for images

--- backend/apps/products/models.py
+++ backend/apps/products/models.py
@@ -8,7 +8,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     active = models.BooleanField(default=True)
     max_per_order = models.IntegerField(null=True, blank=True)
-    image = models.CharField(max_length=2048, null=True, blank=True)
+    image = models.ImageField(upload_to='products/', max_length=2048, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     modifiers = ArrayField(models.CharField(max_length=50), null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
