c6eaacbb596a67137b4bcf51200adefe9d80e00f
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2026-07-24 14:37:56 -0700

    feat: added default path connections and settings

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('apps.users.urls')),
     path('api/menu/', include('apps.products.urls')),
+    path('api/orders/', include('apps.orders.urls')),
 ]
