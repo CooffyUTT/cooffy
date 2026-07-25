1c967e1fb3aef0de7c6cfa679bc73538fb3e2f90
SKSKronosUTT <0323105967@ut-tijuana.edu.mx>
2026-07-25 14:01:13 -0700

    feat: added order admin code to view it on admin panel

--- backend/apps/orders/admin.py
+++ backend/apps/orders/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Order, OrderProduct
 
-# Register your models here.
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'client_id', 'branch_id', 'date', 'total', 'state', 'payment_status', 'created_at',)
+    list_filter = ('state', 'payment_status', 'branch_id', 'payment_method')
+    search_fields = ('order_number', 'client_id')
+    readonly_fields = ('created_at',)
+
+@admin.register(OrderProduct)
+class OrderProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'order', 'item_id', 'quantity', 'price')
+    list_filter = ('item_id',)
+    search_fields = ('order__order_number', 'item_id')
