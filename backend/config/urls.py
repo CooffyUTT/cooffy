85a5452bf600b877dcbe6fb0752832616533b86a
GeneBrit <0323105958@ut-tijuana.edu.mx>
2026-07-28 00:12:39 -0700

    endpoints builded and tested (empy json version :c )

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('api/menu/', include('apps.products.urls')),
     path('api/branches/', include('apps.branches.urls')),
     path('api/orders/', include('apps.orders.urls')),
+    path('api/schools/', include('apps.schools.urls')),
 ]
 
 # Serving static and media files during development
